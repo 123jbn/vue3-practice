63f2d32eb0aecf565e154fa6f69135613a794cb8
贾炳楠 <jbn951216@163.com>
2022-06-07 23:24:56 +0800

    feat:登陆页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,7 +2,7 @@
  * @Author: error: git config user.name && git config user.email & please set dead value or install git
  * @Date: 2022-06-07 21:38:29
  * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2022-06-07 23:06:39
+ * @LastEditTime: 2022-06-07 23:24:41
  * @FilePath: /vue3-demo/src/views/login/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -18,7 +18,7 @@
           <el-input placeholder="请输入密码" />
         </el-form-item>
         <el-form-item>
-          <el-button type="primary">登陆</el-button>
+          <el-button type="primary">登陆111</el-button>
         </el-form-item>
       </el-form>
     </div>
